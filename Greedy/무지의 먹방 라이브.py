039748015b5ef4547318169078103af7a5cacaa6
Ian Choi <yusuk6185@gmail.com>
2021-08-23 15:06:53 +0900

    worked on the difficult question in greedy

--- Greedy/무지의 먹방 라이브.py
+++ Greedy/무지의 먹방 라이브.py
@@ -19,24 +19,31 @@
 import heapq
 
 def solution(food_times, k):
+    # if ate all the foods before the connection loss print -1
     if sum(food_times) <= k:
         return -1
 
     q = []
+    # insert all the foods in to the queue
     for i in range(len(food_times)):
-        heapq.heappush(q, (food_times[i], i + 1))
+        heapq.heappush(q, (food_times[i], i + 1)) # prioritized by the food time and labelled with food number
     
+    # time taken to eat
     sum_value = 0
+    # time taken to finish previous food
     previous = 0
+    # number of food left
     length = len(food_times)
 
+    # compare k with sum_value + (current food time - previous food time) * current number of food 
     while sum_value + ((q[0][0] - previous) * length) <= k:
         now = heapq.heappop(q)[0]
         sum_value += (now - previous) * length
-        length -= 1
-        previous = now
+        length -= 1 # subtract the food finished
+        previous = now # reset the previous food time
 
-    result = sorted(q, key = lambda x: x[1])
+    # print the food number among the food left
+    result = sorted(q, key = lambda x: x[1]) # sort the food by the food number
     return result[(k - sum_value) % length][1]
 
 print(solution([3,1,2],5))
