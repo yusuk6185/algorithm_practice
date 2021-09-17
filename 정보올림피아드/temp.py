0df39e270daa95f4dd8c28c527630f50e2a60b87
Ian Choi <yusuk6185@gmail.com>
2021-09-17 23:54:05 +0900

    9/17 problem solving

--- 정보올림피아드/temp.py
+++ 정보올림피아드/temp.py
@@ -1,33 +1,22 @@
-INF = int(1e9)
 
 n = int(input())
-a = [[INF] * (n+1) for _ in range(n+1)]
 
-while True:
-    s1, s2 = map(int, input().split())
+max_l = 0
+max_list = []
 
-    if s1 == -1 and s2 == -2:
-        break
-    a[s1][s2] = 1
-    a[s2][s1] = 1
+for i in range(n+1):
+    result = [n, i]
+    j = 0
+    while True:
+        new_num = result[j] - result[j+1]
+        if new_num <= -1:
+            break
+        result.append(new_num)
+        if max_l < len(result):
+            max_l = len(result)
+            max_list = result
+        j += 1
 
-for i in range(1, n+1):
-    a[i][i] = 0
-
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if a[i][j] == 1 or a[i][j] == 0:
-                continue
-            elif a[i][j] > a[i][k] + a[k][j]:
-                a[i][j] = a[i][k] + a[k][j]
-
-r = []
-for i in range(1, n+1):
-    r.append(max(a[i][1:]))
-
-m = min(r)
-print(m, r.count(m))
-for i,v in enumerate(r):
-    if v == m:
-        print(i+1, end = ' ')
+print(max_l)
+for i in max_list:
+    print(i, end = ' ')
