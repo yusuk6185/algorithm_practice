6ec08912de69179c4a14f021aeb10bf939df7259
Ian Choi <yusuk6185@gmail.com>
2021-09-30 23:55:54 +0900

    9/30 problem solving

--- 정보올림피아드/temp.py
+++ 정보올림피아드/temp.py
@@ -1,23 +1,3 @@
-from collections import deque
+n = input()[1:-1].split(',')
 
-n, m = map(int, input().split())
-pos = list(map(int, input().split()))
-queue = deque([i for i in range(1, n+1)])
-
-count = 0
-for i in pos:
-    while True:
-        if queue[0] == i:
-            queue.popleft()
-            break
-        else:
-            if queue.index(i) < len(queue)/2:
-                while queue[0] != i:
-                    queue.append(queue.popleft())
-                    count += 1
-            else:
-                while queue[0] != i:
-                    queue.appendleft(queue.pop())
-                    count += 1
-
-print(count)
+print(n)
