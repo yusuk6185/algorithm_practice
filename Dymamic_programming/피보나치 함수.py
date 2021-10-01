71bca53986857b240b50051e2b2ec5e32c61d90a
Ian Choi <yusuk6185@gmail.com>
2021-10-01 20:48:02 +0900

    10/1 problem solving

--- Dymamic_programming/피보나치 함수.py
+++ Dymamic_programming/피보나치 함수.py
@@ -1,19 +1,17 @@
 # https://www.acmicpc.net/problem/1003
 
-a = int(input())
- 
-zero = [1,0,1]
-one = [0,1,1]
- 
-def cal(num):
-    length = len(zero)
-    if length <= num:
-        for i in range(length,num+1):
-            zero.append(zero[i-1]+zero[i-2])
-            one.append(one[i-1]+one[i-2])
-    print("%d %d"%(zero[num],one[num]))
- 
-for i in range(a):
-    k = int(input())
-    cal(k)
+zero = [1, 0, 1]
+one = [0, 1, 1]
 
+def fibo(n):
+    length = len(zero)
+    if length <= n:
+        for i in range(length, n+1):
+            zero.append(zero[i-1] + zero[i-2])
+            one.append(one[i-1] + one[i-2])
+    print('{} {}'.format(zero[n], one[n]))
+
+t = int(input())
+
+for i in range(t):
+    fibo(int(input()))
