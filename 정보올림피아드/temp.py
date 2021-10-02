30c32d6931864661de260ff563d580c9e97beb5c
Ian Choi <yusuk6185@gmail.com>
2021-10-02 23:05:19 +0900

    10/2 problem solving

--- 정보올림피아드/temp.py
+++ 정보올림피아드/temp.py
@@ -1,14 +1,17 @@
-n= int(input())
+dp = [0 for _ in range(301)]
+arr = [0 for _ in range(301)]
 
-dp = [0 for _ in range(n+1)]
+n = int(input())
+
+for i in range(n):
+    arr[i] = int(input())
+
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
 
 for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
-    
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-    
-print(dp[n])
+    dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i])
+
+print(dp[n-1])
+
+print(dp)
